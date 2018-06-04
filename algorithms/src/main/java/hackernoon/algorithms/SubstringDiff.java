3c6a83783e7afe492292490c73d35b76fe4456ad
Adam Myers <myers.adam.k@gmail.com>
2018-06-04 13:12:26 -0400

    Added some documentation/Big-O analysis for algo

--- algorithms/src/main/java/hackernoon/algorithms/SubstringDiff.java
+++ algorithms/src/main/java/hackernoon/algorithms/SubstringDiff.java
@@ -1,8 +1,5 @@
 package hackernoon.algorithms;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public final class SubstringDiff {
 
   private final String lhs;
@@ -13,59 +10,26 @@ public final class SubstringDiff {
     this.rhs = rhs.trim();
   }
 
-  public int fme(int k, String lhs, String rhs) {
-    final int maxLength = Integer.max(lhs.length(), rhs.length());
-    final Set<Integer> matchingSubstringsLengths = new HashSet<>(maxLength);
-
-    for (int lhsStartIndex = 0; lhsStartIndex < maxLength; lhsStartIndex++) {
-      for (int rhsIndex = 0; rhsIndex < maxLength; rhsIndex++) {
-        matchingSubstringsLengths.add(
-            fme(k, lhs.substring(lhsStartIndex, maxLength), rhs.substring(rhsIndex, maxLength))
-        );
-      }
-    }
-    return matchingSubstringsLengths.stream().max(Integer::compareTo).get();
-  }
-
-  public int substringDiffer(int k) {
-    if (lhs.equals(rhs)) {
-      return lhs.length();
-    }
-
-    final int maxLength = Integer.max(lhs.trim().length(), rhs.trim().length());
-    final Set<Integer> matchingSubstringsLengths = new HashSet<>(maxLength);
-
-    int currentSubstringLength = 0;
-    int differingCharacters = 0;
-
-    for (int lhsStartIndex = 0; lhsStartIndex < maxLength; lhsStartIndex++) {
-      for (int lhsIndex = 0; lhsIndex < maxLength; lhsIndex++) {
-        final int rhsStartIndex = rhs.indexOf(lhs.charAt(lhsIndex), lhsStartIndex);
-
-        while (rhsStartIndex > 0) {
-          for (int rhsIndex = rhsStartIndex; rhsIndex < maxLength; rhsIndex++) {
-            if (lhs.charAt(lhsIndex) != rhs.charAt(rhsIndex)) {
-              differingCharacters++;
-            }
-
-            if (differingCharacters > k) {
-              matchingSubstringsLengths.add(currentSubstringLength);
-
-              currentSubstringLength = 0;
-              differingCharacters = 0;
-            } else {
-              currentSubstringLength++;
-            }
-          }
-        }
-      }
-//      for (int)
-    }
-//      for (int rhsStartIndex = 0; rhsStartIndex < maxLength;) {
-//        for (int lhsIndex = lhsStartIndex, rhsIndex = rhsStartIndex; lhsIndex < maxLength;) {
-    return 0;
-  }
-
+  /**
+   * Big-O is tough for this optimization. I believe the worst-case scenario is where k=1, and every
+   * other character from {@code lhs} and {@code rhs} match, so that the algorithm has to keep going
+   * to see if it can reach a higher max, but it repeatedly restarts. With k=1, the amount of
+   * restarts will be maximized.
+   * <p />
+   * Furthermore, for each iteration on n, the size of rhs shrinks by 2, since we ignore the first n
+   * positions that have already been considered.
+   * <p />
+   * I believe that is something like this:
+   * <ol>
+   * <li>{@code O(n^({n/2, n-2/2, n-4/2, ...1}))}</li>
+   * <li>{@code O(n^(n^log(n))+1)}</li>
+   * <li>{@code O(n^(n^log(n)))}</li>
+   * </ol>
+   * Although I think that's leaving out k, which has a place in the optimization, as well.
+   *
+   * @param k
+   * @return
+   */
   public int substringDiff(int k) {
     final int maxLength = Integer.max(lhs.trim().length(), rhs.trim().length());
 
@@ -74,19 +38,26 @@ public final class SubstringDiff {
     int currentMax = 0;
 
     for (int lhsStartIndex = 0; lhsStartIndex < maxLength; lhsStartIndex++) {
-
       if (currentMax > maxLength - lhsStartIndex) {
         return currentMax;
       }
 
-      // TODO: this substring is superfluous, if indices can be managed properly.
+      // The "anchor" strings are created to make zero-based indexing intuitive through the rest of
+      // the admittedly complex algorithm. For the loop that does the actual comparisons, it will
+      // always index based on the substrings beginning with lhsStart and rhsStart, where rhsStart
+      // is always an index matching lhsStart[0].
+
       final String lhsAnchor = lhs.substring(lhsStartIndex, maxLength);
 
       for (int rhsStartIndex = rhs.indexOf(lhsAnchor.charAt(0));
                rhsStartIndex >= 0;) {
+
         final String rhsAnchor = rhs.substring(rhsStartIndex, maxLength);
 
-        for (int lhsIndex = 0, rhsIndex = 0; lhsIndex < lhsAnchor.length() && rhsIndex < rhsAnchor.length(); lhsIndex++, rhsIndex++) {
+        for (int lhsIndex = 0, rhsIndex = 0;
+                 lhsIndex < lhsAnchor.length() && rhsIndex < rhsAnchor.length();
+                 lhsIndex++, rhsIndex++) {
+
           if (lhsAnchor.charAt(lhsIndex) != rhsAnchor.charAt(rhsIndex)) {
             differingCharacters++;
           }
@@ -94,13 +65,14 @@ public final class SubstringDiff {
           if (differingCharacters > k) {
             break;
           }
-          currentSubstringLength++;
+          if (++currentSubstringLength == maxLength) {
+            return maxLength;
+          }
         }
 
         if (differingCharacters < k) {
            currentSubstringLength++;
         }
-
         if (currentSubstringLength > currentMax) {
           currentMax = currentSubstringLength;
         }
@@ -110,7 +82,6 @@ public final class SubstringDiff {
         rhsStartIndex = rhs.indexOf(lhsAnchor.charAt(0), rhsStartIndex + 1);
       }
     }
-
     return currentMax;
   }
 }

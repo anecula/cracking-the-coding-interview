99ffb65befc8de5ffb2a4ebdb08ebe5a64b0de6c
Adam Myers <myers.adam.k@gmail.com>
2018-06-16 08:47:18 -0400

    Improved power of World API by adding functional concepts. Fixed World to update Organisms properly per iteration.

--- games/src/main/java/World.java
+++ games/src/main/java/World.java
@@ -2,33 +2,40 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public final class World {
 
   private final List<List<Organism>> board;
 
-  public static World createRandomized(int n) {
-    assert n > 0 : "n <= 0";
+  public static final int MINIMUM_N = 3;
 
-    final Random random = new Random(n);
+  public static World create(int n, BiFunction<Integer, Integer, Organism> factory) {
+    assert n >= MINIMUM_N : "Requires at least a 3x3 board (given " + n + ")";
+
     final List<List<Organism>> board = new ArrayList<>(n);
 
     // TODO: I think this can be improved using IntStream.range, but may be dubiously better.
     for (int i = 0; i < n; i++) {
       board.add(new ArrayList<>(n));
       for (int k = 0; k < n; k++) {
-        board.get(i).add(new Organism(random.nextBoolean(), i, k));
+        board.get(i).add(factory.apply(i, k));
       }
     }
     return new World(board);
   }
 
   public static World create(Collection<Organism> organisms) {
-    assert organisms != null && !organisms.isEmpty() : "organisms == null || organisms.isEmpty()";
+    assert organisms != null : "organisms cannot be null";
+    assert
+        organisms.size() >= MINIMUM_N * MINIMUM_N :
+        "Requires at least 9 organisms (given " + organisms.size() + ")";
 
     final Map<Integer, List<Organism>> organismsByAxis =
         organisms.stream()
@@ -42,14 +49,28 @@ public final class World {
     );
   }
 
-  public Map<Organism, Integer> getAdjacentLivingOrganisms() {
+  public void updateOrganism(int x, int y, Consumer<Organism> applicator) {
+    assert x < board.size() && x >= 0;
+    Organism organism = board.get(x).get(y);
+    board.get(x).remove(organism);
+
+    applicator.accept(organism);
+    board.get(x).add(y, organism);
+  }
+
+  public Organism getOrganism(int x, int y) {
+    assert x < board.size() && x >= 0;
+    return board.get(x).get(y);
+  }
+
+  public Map<Organism, Integer> getAdjacentLivingOrganismCountByOrganism() {
     return board.stream()
         .flatMap(List::stream)
         .collect(Collectors.toList()).stream()
         .collect(Collectors.toMap(Function.identity(), this::countAdjacentLivingOrganisms));
   }
 
-  private int countAdjacentLivingOrganisms(Organism organism) {
+  private int countAdjacentLivingOrganisms(final Organism organism) {
     final int x = organism.xIndex();
     final int y = organism.yIndex();
 
@@ -63,8 +84,8 @@ public final class World {
         if (k < 0 || k >= board.size()) {
           continue;
         }
-        // TODO: this function could be made more functional: given a function, apply it to all
-        // neighbors of each element, and return a Collection contains the results.
+        // TODO: this method could be made more functional: given a function, apply it to all, and
+        // accumulate the results in a Collection.
         if (board.get(i).get(k).isAlive()) {
           adjacentLivingOrganisms++;
         }
@@ -87,11 +108,44 @@ public final class World {
   }
 }
 
+interface OrganismFactory extends BiFunction<Integer, Integer, Organism> {
+
+  @Override
+  default Organism apply(Integer x, Integer y) {
+    return new Organism(false, x, y);
+  }
+}
+
+final class RandomizedOrganismFactory implements OrganismFactory {
+
+  private static final Random RANDOM = new Random();
+
+  @Override
+  public Organism apply(Integer x, Integer y) {
+    return new Organism(RANDOM.nextBoolean(), x, y);
+  }
+}
 
 @FunctionalInterface
 interface GameOfLife {
 
-  Stream<World> iterate(int iterationCount);
+  List<World> iterate(int iterationCount);
+}
+
+abstract class GameOfLifeDecorator<T extends GameOfLife> implements GameOfLife {
+
+  private final T game;
+
+  public GameOfLifeDecorator(T game) {
+    this.game = game;
+  }
+
+  @Override
+  public List<World> iterate(int iterationCount) {
+    final List<World> iterationResults = game.iterate(iterationCount);
+    iterationResults.stream(); // print each step
+    return null;
+  }
 }
 
 final class ConwaysGame implements GameOfLife {
@@ -103,18 +157,23 @@ final class ConwaysGame implements GameOfLife {
   }
 
   @Override
-  public Stream<World> iterate(int iterationCount) {
-//    return World.create(
-//        world.getAdjacentLivingOrganisms().entrySet().stream()
-//        .map(entry ->
-//          applyRule(entry.getKey(), entry.getValue())
-//        ).collect(Collectors.toList()));
-
-    // TODO
-    return null;
+  public List<World> iterate(int iterationCount) {
+    final List<World> iterations = new ArrayList<>(iterationCount);
+    for (int i = 0; i < iterationCount; i++) {
+      iterations.add(
+          World.create(world.getAdjacentLivingOrganismCountByOrganism()
+              .entrySet().stream()
+              .map(this::applyRule)
+              .collect(Collectors.toList()))
+      );
+    }
+    return iterations;
   }
 
-  private Organism applyRule(final Organism organism, final int adjacentLivingOrganisms) {
+  private Organism applyRule(Entry<Organism, Integer> entry) {
+    final Organism organism = entry.getKey();
+    final int adjacentLivingOrganisms = entry.getValue();
+
     if (organism.isAlive() && adjacentLivingOrganisms < 2 || adjacentLivingOrganisms > 3) {
       organism.kill();
     } else if (!organism.isAlive() && adjacentLivingOrganisms == 3) {
--- games/src/test/java/WorldTest.java
+++ games/src/test/java/WorldTest.java
@@ -1,12 +1,58 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class WorldTest {
 
   @Test
   void createRandomized() {
-    new ConwaysGame(World.createRandomized(10)).iterate(10).forEach(world -> {
-      System.out.println(world);
-      System.out.println("=======================");
-    });
+    new ConwaysGame(World.create(10, (i, j) -> new Organism(false, i, j))).iterate(10)
+        .forEach(world -> {
+
+        });
+    new ConwaysGame(World.create(10, new RandomizedOrganismFactory())).iterate(10)
+        .forEach(world -> {
+          System.out.println(world);
+          System.out.println("=======================");
+        });
   }
+
+  @Test
+  void resurrectOrganism() {
+    final World world = World.create(
+        World.MINIMUM_N, new SelectiveOrganismFactory(Map.of(1, 1))
+    );
+
+    assertThat(world.getOrganism(1, 1).isAlive(), is(false));
+    world.updateOrganism(1, 1, Organism::resurrect);
+    assertThat(world.getOrganism(1, 1).isAlive(), is(true));
+  }
+
+  @Test
+  void underPopulation() {
+    final World world = World.create(
+        World.MINIMUM_N, new SelectiveOrganismFactory(Map.of(1, 1))
+    );
+    world.updateOrganism(1, 1, Organism::resurrect);
+
+    final List<World> iterations = new ConwaysGame(world).iterate(1);
+    assertThat(iterations.size(), is(1));
+    assertThat(iterations.get(0).getOrganism(1, 1).isAlive(), is(false));
+  }
+
+  private static class SelectiveOrganismFactory implements OrganismFactory {
+    private final Map<Integer, Integer> aliveSpaces;
+
+    public SelectiveOrganismFactory(Map<Integer, Integer> aliveSpaces) {
+      this.aliveSpaces = aliveSpaces;
+    }
+
+    @Override
+    public Organism apply(Integer x, Integer y) {
+      return new Organism(false, x, y);
+    }
+  };
 }
